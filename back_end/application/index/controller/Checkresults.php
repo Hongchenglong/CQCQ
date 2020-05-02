330a10f76c7cd15dd2a39a5abf472fd168819c21
Hongchenglong <844651670@qq.com>
2020-05-02 23:34:45 +0800

    删除查寝记录

--- back_end/application/index/controller/Checkresults.php
+++ back_end/application/index/controller/Checkresults.php
@@ -51,7 +51,47 @@ class Checkresults extends BaseController
         }
     }
 
-    
+    /**
+     * 删除查寝记录
+     */
+    public function deleteRecord() {
+        // 校验参数是否存在
+        $parameter = array();
+        $parameter = ['department', 'grade', 'start_time'];
+        $result = $this->checkForExistence($parameter);
+        if ($result) {
+            return $result;
+        }
+
+        // 查询条件
+        $where = array();
+        $where['s.grade'] = $_POST['grade'];
+        $where['s.department'] = $_POST['department'];
+
+        $result = Db::table('record')
+            ->alias('r')    // 别名
+            ->join('dorm d', 'd.id = r.dorm_id')
+            ->join('student s', 's.id = d.student_id')
+            ->where('confirmed = 1')
+            ->where($where)
+            ->update(['record.deleted' => 1]);
+
+        if ($result) {
+            $return_data = array();
+            $return_data['error_code'] = 0;
+            $return_data['msg'] = '删除查寝记录' . $result . '条';
+            // $return_data['data'] = $result;
+
+            return json($return_data);
+        } else {
+            $return_data = array();
+            $return_data['error_code'] = 2;
+            $return_data['msg'] = '无此时间的查寝记录';
+
+            return json($return_data);
+        }
+    }
+
 
 
 
