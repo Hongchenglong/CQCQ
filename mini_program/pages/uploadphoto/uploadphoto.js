3781b1e2648d20831f731571729c041116c9d925
Hongchenglong <844651670@qq.com>
2020-05-25 22:46:45 +0800

    上传照片

--- mini_program/pages/uploadphoto/uploadphoto.js
+++ mini_program/pages/uploadphoto/uploadphoto.js
@@ -5,21 +5,21 @@ Page({
     pics: [],
     isShow: true
   },
-  Img:function(){
+  Img: function () {
     var that = this;
-    var imgSrc= getApp().globalData.imgSrc;
-  
-    if(imgSrc!=''){
-    upload(that, imgSrc);
-    }else{
+    var imgSrc = getApp().globalData.imgSrc;
+
+    if (imgSrc != '') {
+      upload(that, imgSrc);
+    } else {
       wx.showToast({
         title: '照片不能为空',
         icon: 'none',
         duration: 1000
       })
     }
-   
-    
+
+
   },
   /**上传图片 */
   uploadImage: function () {
@@ -42,9 +42,9 @@ Page({
         that.setData({
           pics: pics
         })
-        
+
       },
-     
+
     })
 
   },
@@ -63,7 +63,7 @@ Page({
     }
     that.setData({
       pics: newPics,
-      
+
       isShow: true
     })
     getApp().globalData.imgSrc1 = newPics
