b330204e5e4f03c8d2365108069faffc13ecc59b
Viola-Violet <979505199@qq.com>
2020-05-27 23:55:07 +0800

    关于

--- mini_program/app.js
+++ mini_program/app.js
@@ -1,6 +1,19 @@
 //app.js
 App({
   onLaunch: function () {
+      wx.getSystemInfo({
+        success: e => {
+          this.globalData.StatusBar = e.statusBarHeight;
+          let capsule = wx.getMenuButtonBoundingClientRect();
+      if (capsule) {
+         this.globalData.Custom = capsule;
+        this.globalData.CustomBar = capsule.bottom + capsule.top - e.statusBarHeight;
+      } else {
+        this.globalData.CustomBar = e.statusBarHeight + 50;
+      }
+        }
+      })
+
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
