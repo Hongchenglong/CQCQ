5174d4a628b5b5427810c3130c292c6f287b52a1
Hongchenglong <844651670@qq.com>
2020-05-29 15:11:20 +0800

    回收站分页

--- back_end/application/index/controller/Recyclebin.php
+++ back_end/application/index/controller/Recyclebin.php
@@ -38,6 +38,7 @@ class Recyclebin extends BaseController
             ->where($where)
             ->where('r.deleted', 1)
             ->order('start_time desc')
+            ->page($_POST['page'] - 1, 5)    // page('第几页','每页显示的数量')
             ->select();
 
         if ($record) {
@@ -226,7 +227,6 @@ class Recyclebin extends BaseController
             ->join('student s', 's.id = d.student_id')
             ->where($where)
             ->where('r.deleted', 1)
-            ->page($_POST['page'] - 1, 5)    // page('第几页','每页显示的数量')
             ->select();
 
         if ($record) {
