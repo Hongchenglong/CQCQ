a82c834d2691893e6fa336eb45680c3735d51d8a
吴娉婷 <60504192+MiFan-ya@users.noreply.github.com>
2020-05-29 17:46:12 +0800

    Update recycle.js

--- mini_program/pages/recycle/recycle.js
+++ mini_program/pages/recycle/recycle.js
@@ -1,31 +1,34 @@
 // pages/chakan/chakan.js
+var page = getApp().globalData.pagetwo; //页
+
 Page({
   /**
    * 页面的初始数据
    */
-  data:{
-    showData:{},
-    dateValue:" - - ",
-    department:'',
-    grade:''
+  data: {
+    showData: [],
+    dateValue: " - - ",
+    department: '',
+    grade: '',
+    loadMoreText: '加载更多',
   },
 
 
-  datePickerBindchange:function(e){
+  datePickerBindchange: function (e) {
     this.setData({
-     dateValue:e.detail.value
+      dateValue: e.detail.value
     })
-   },
+  },
 
-   //搜索记录
-   onSearch:function(e){
-     var that = this
+  //搜索记录
+  onSearch: function (e) {
+    var that = this
     wx.request({
       url: getApp().globalData.server + '/cqcq/public/index.php/index/Recyclebin/specifiedDeletedDate',
       data: {
-        department:that.data.department,
-        grade:that.data.grade,
-        date:that.data.dateValue
+        department: that.data.department,
+        grade: that.data.grade,
+        date: that.data.dateValue
       },
       method: "POST",
       header: {
@@ -37,18 +40,16 @@ Page({
             title: '提示！',
             showCancel: false,
             content: res.data.msg,
-            success: function (res) { }
+            success: function (res) {}
           })
-        }
-        else if (res.data.error_code == 2) {
+        } else if (res.data.error_code == 2) {
           wx.showModal({
             title: '提示！',
             showCancel: false,
             content: res.data.msg,
-            success: function (res) { }
+            success: function (res) {}
           })
-        }
-        else if (res.data.error_code != 0) {
+        } else if (res.data.error_code != 0) {
           wx.showModal({
             title: '哎呀～',
             content: '出错了呢！' + res.data.msg,
@@ -60,10 +61,9 @@ Page({
               }
             }
           })
-        }
-        else if (res.data.error_code == 0) {
+        } else if (res.data.error_code == 0) {
           that.setData({
-            showData:res.data.data
+            showData: res.data.data
           })
           console.log(that.data.showData)
         }
@@ -81,20 +81,23 @@ Page({
           }
         })
       },
-      complete:function(res){
+      complete: function (res) {
         wx.hideLoading()
       }
     })
-   },
+  },
 
-   //全部记录
-   onAll:function(e){
-     var that = this
-     that.onLoad()
-   },
+  //全部记录
+  onAll: function (e) {
+    this.setData({
+      showData: [],
+    })
+    getApp().globalData.pagetwo = 2
+    this.onLoad();
+  },
 
-   //获取全部记录
-  onLoad: function(options) {
+  //获取全部记录
+  getList: function (page) {
     //获取当前时间戳  
     var timestamp = Date.parse(new Date());
     timestamp = timestamp / 1000;
@@ -113,7 +116,7 @@ Page({
     var m = date.getMinutes();
     //秒  
     var s = date.getSeconds();
-    var time =  Y + "-" + M + "-" + D + " " + h + ":" + m + ":" + s;
+    var time = Y + "-" + M + "-" + D + " " + h + ":" + m + ":" + s;
     this.setData({
       time: time
     })
@@ -121,15 +124,16 @@ Page({
       grade: getApp().globalData.user.grade,
       department: getApp().globalData.user.department
     })
-    var that=this
+    var that = this
     wx.showLoading({
       title: '加载中',
     })
     wx.request({
       url: getApp().globalData.server + '/cqcq/public/index.php/index/Recyclebin/checkDeletedRecords',
       data: {
-        department:that.data.department,
-        grade:that.data.grade
+        department: that.data.department,
+        grade: that.data.grade,
+        page: page
       },
       method: "POST",
       header: {
@@ -141,18 +145,16 @@ Page({
             title: '提示！',
             showCancel: false,
             content: res.data.msg,
-            success: function (res) { }
+            success: function (res) {}
           })
-        }
-        else if (res.data.error_code == 2) {
-          wx.showModal({
-            title: '提示！',
-            showCancel: false,
-            content: res.data.msg,
-            success: function (res) { }
-          })
-        }
-        else if (res.data.error_code != 0) {
+        } else if (res.data.error_code == 2) {
+          // wx.showModal({
+          //   title: '提示！',
+          //   showCancel: false,
+          //   content: res.data.msg,
+          //   success: function (res) { }
+          // })
+        } else if (res.data.error_code != 0) {
           wx.showModal({
             title: '哎呀～',
             content: '出错了呢！' + res.data.msg,
@@ -164,12 +166,22 @@ Page({
               }
             }
           })
-        }
-        else if (res.data.error_code == 0) {
-          that.setData({
-            showData: res.data.data,
-          })
+        } else if (res.data.error_code == 0) {
+          //懒加载
+          getApp().globalData.pagetwo += 1
+          if (res.data.data.length > 0) {
+            that.setData({
+              showData: that.data.showData.concat(res.data.data) //合并数据
+            })
+          } else {
+            that.setData({
+              loadMoreText: '没有数据了'
+            })
+          }
+          
           console.log(that.data.showData)
+          console.log(res)
+          console.log(res.data.data)
         }
       },
       fail: function (res) {
@@ -185,58 +197,84 @@ Page({
           }
         })
       },
-      complete:function(res){
-        wx.hideLoading()
-      }
+      // complete:function(res){
+      //   wx.hideLoading()
+      // }
     })
-    setTimeout(function() {
+    setTimeout(function () {
       wx.hideLoading()
-    },2000)
+    }, 2000)
   },
-  
+
   //恢复记录
   onLike: function (e) {
-    var that=this
+    var that = this
     wx.showModal({
       title: '提示',
       content: '您确认恢复此记录？',
       success: function (res) {
-      if (res.confirm) {
-        console.log('用户点击确定')
-        wx.request({
-          url: getApp().globalData.server + '/cqcq/public/index.php/index/Recyclebin/recoverRecord',
-          data: {
-            department:that.data.department,
-            grade:that.data.grade,
-            start_time:e.target.dataset.start_time,
-            end_time:e.target.dataset.end_time
-          },
+        if (res.confirm) {
+          console.log('用户点击确定')
+          wx.request({
+            url: getApp().globalData.server + '/cqcq/public/index.php/index/Recyclebin/recoverRecord',
+            data: {
+              department: that.data.department,
+              grade: that.data.grade,
+              start_time: e.target.dataset.start_time,
+              end_time: e.target.dataset.end_time
+            },
 
-          method: "POST",
-          header: {
-            "Content-Type": "application/x-www-form-urlencoded"
-          },
-          success: function (res) {
-            if (res.data.error_code == 1) {
-              wx.showModal({
-                title: '提示！',
-                showCancel: false,
-                content: res.data.msg,
-                success: function (res) { }
-              })
-            }
-            else if (res.data.error_code == 2) {
-              wx.showModal({
-                title: '提示！',
-                showCancel: false,
-                content: res.data.msg,
-                success: function (res) { }
-              })
-            }
-            else if (res.data.error_code != 0) {
+            method: "POST",
+            header: {
+              "Content-Type": "application/x-www-form-urlencoded"
+            },
+            success: function (res) {
+              if (res.data.error_code == 1) {
+                wx.showModal({
+                  title: '提示！',
+                  showCancel: false,
+                  content: res.data.msg,
+                  success: function (res) {}
+                })
+              } else if (res.data.error_code == 2) {
+                wx.showModal({
+                  title: '提示！',
+                  showCancel: false,
+                  content: res.data.msg,
+                  success: function (res) {}
+                })
+              } else if (res.data.error_code != 0) {
+                wx.showModal({
+                  title: '哎呀～',
+                  content: '出错了呢！' + res.data.msg,
+                  success: function (res) {
+                    if (res.confirm) {
+                      console.log('用户点击确定')
+                    } else if (res.cancel) {
+                      console.log('用户点击取消')
+                    }
+                  }
+                })
+              } else if (res.data.error_code == 0) {
+                wx.showModal({
+                  success: function (res) {
+                    if (res.confirm) {
+                      console.log('用户点击确定')
+                      that.onAll()
+                    } else if (res.cancel) {
+                      console.log('用户点击取消')
+                    }
+                  },
+                  title: '恭喜！',
+                  showCancel: false,
+                  content: '恢复成功',
+                })
+              }
+            },
+            fail: function (res) {
               wx.showModal({
                 title: '哎呀～',
-                content: '出错了呢！' + res.data.msg,
+                content: '网络不在状态呢！',
                 success: function (res) {
                   if (res.confirm) {
                     console.log('用户点击确定')
@@ -245,44 +283,13 @@ Page({
                   }
                 }
               })
-            }
-            else if (res.data.error_code == 0) {
-          wx.showModal({
-            success: function (res) {
-              if (res.confirm) {
-                console.log('用户点击确定')
-                that.onLoad()
-              } else if (res.cancel) {
-                console.log('用户点击取消')
-              }
             },
-            title: '恭喜！',
-            showCancel: false,
-            content: '恢复成功',
+            complete: function (res) {}
           })
+        } else if (res.cancel) {
+          console.log('用户点击取消')
         }
-          },
-          fail: function (res) {
-            wx.showModal({
-              title: '哎呀～',
-              content: '网络不在状态呢！',
-              success: function (res) {
-                if (res.confirm) {
-                  console.log('用户点击确定')
-                } else if (res.cancel) {
-                  console.log('用户点击取消')
-                }
-              }
-            })
-          },
-          complete:function(res){
-          }
-        })
       }
-      else if (res.cancel) {
-        console.log('用户点击取消')
-      }
-    }
     })
   },
 
@@ -291,5 +298,26 @@ Page({
     wx.navigateTo({
       url: "../recycle_bin/recycle_bin?time=" + e.target.dataset.times + "&&endtime=" + e.target.dataset.endtime
     })
+  },
+  //显示
+  onLoad: function (options) {
+    // 页面初始化 options为页面跳转所带来的参数
+    var that = this
+    that.getList(1)
+    
+    // if (getApp().globalData.all == '') {
+    //   wx.showModal({
+    //     title: '提示！',
+    //     showCancel: false,
+    //     content: '回收站为空'
+    //   })
+    // }
+  },
+  //触底
+  onScrollLower: function () {
+    var that = this
+    page = getApp().globalData.pagetwo + 1;
+    console.log(page)
+    that.getList(page)
   },
 })
