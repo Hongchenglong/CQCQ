ef0d7e8b060ac257bfdf09c50e4c200b668da2e2
Viola-Violet <979505199@qq.com>
2020-06-01 10:20:00 +0800

    意见反馈

--- mini_program/pages/teacher_mine/teacher_mine.js
+++ mini_program/pages/teacher_mine/teacher_mine.js
@@ -134,6 +134,7 @@ Component({
         department: getApp().globalData.user.department
       })
       console.log("show")
+      wx.hideLoading()
     },
   },
 
@@ -191,90 +192,78 @@ Component({
     },
 
     to_info:function(){
-      wx.showToast({
-        title: '加载中...',
+      wx.showLoading({
+        title: '加载中',
         mask: true,
-        icon: 'loading',
-        duration: 400
-        })
+      })
       wx.navigateTo({
         url:"../teacher_information/teacher_information" 
       })
+      wx.hideLoading()
     },
 
     to_pass:function(){
-      wx.showToast({
-        title: '加载中...',
+      wx.showLoading({
+        title: '加载中',
         mask: true,
-        icon: 'loading',
-        duration: 400
-        })
+      })
       wx.navigateTo({
         url:"../revise_password/revise_password"
       })
+      wx.hideLoading()
     },
 
     to_mail:function(){
-      wx.showToast({
-        title: '加载中...',
+      wx.showLoading({
+        title: '加载中',
         mask: true,
-        icon: 'loading',
-        duration: 400
-        })
+      })
       wx.navigateTo({
         url:"../revise_email/revise_email"
       })
+      wx.hideLoading()
     },
 
     to_phone:function(){
-      wx.showToast({
-        title: '加载中...',
+      wx.showLoading({
+        title: '加载中',
         mask: true,
-        icon: 'loading',
-        duration: 400
-        })
+      })
       wx.navigateTo({
         url:"../revise_phone/revise_phone"
       })
+      wx.hideLoading()
     },
 
     to_re:function(){
-      wx.showToast({
-        title: '加载中...',
+      wx.showLoading({
+        title: '加载中',
         mask: true,
-        icon: 'loading',
-        duration: 400
-        })
+      })
       wx.navigateTo({
         url:"../recycle/recycle"
       })
-    },
-
-    to_su:function(){
-      open-type
+      wx.hideLoading()
     },
 
     to_about:function(){
-      wx.showToast({
-        title: '加载中...',
+      wx.showLoading({
+        title: '加载中',
         mask: true,
-        icon: 'loading',
-        duration: 400
-        })
+      })
       wx.navigateTo({
         url:"../revise_about/revise_about"
       })
+      wx.hideLoading()
     },
 
      //点击加载样式
      click: function () {
       //加载中的样式
-      wx.showToast({
-        title: '加载中...',
+      wx.showLoading({
+        title: '加载中',
         mask: true,
-        icon: 'loading',
-        duration: 400
-        })
+      })
     },
 
     //事件处理函数
