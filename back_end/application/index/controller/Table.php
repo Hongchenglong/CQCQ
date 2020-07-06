02770b8315c1782c91596994c22322f0795d3bf5
crush-w <877180727@qq.com>
2020-07-06 19:15:38 +0800

    导入csv文件

--- back_end/application/index/controller/Table.php
+++ back_end/application/index/controller/Table.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace app\index\controller;
 
 use \think\Db;
@@ -23,7 +22,7 @@ class Table extends BaseController
 		return $this->fetch();
 	}
 
-	public function hint()
+	public function file()
 	{
 		return $this->fetch();
 	}
@@ -60,10 +59,7 @@ class Table extends BaseController
 			$return_data['data'] = $data;
 			return json($return_data['data']);
 		} else {
-			$return_data = array();
-			$return_data['error_code'] = 1;
-			$return_data['msg'] = '查无该学号！';
-			return json($return_data);
+			echo "查无该学号";
 		}
 	}
 
@@ -96,52 +92,150 @@ class Table extends BaseController
 			];
 
 		if ($data_id) {
-			echo "false";
-			$return_data = array();
-			$return_data['error_code'] = 1;
-			$return_data['title'] = '该学号已存在';
-			$return_data['data'] = 'k';
-			$this->assign("data", $return_data);
-			$this->fetch('hint');
+			echo "<script language=\"JavaScript\">\r\n";
+			echo " alert(\"添加失败，该学号已存在\");\r\n";
+			echo " history.back();\r\n";
+			echo "</script>";
 		} else {
-			echo "true";
 			Db('Student')->insert($data);
-			$return_data = array();
-			$return_data['title'] = '添加成功！';
-			$return_data['data'] = $data;
-			$this->assign("data", $return_data);
-			$this->fetch('hint');
+			echo "<script language=\"JavaScript\">\r\n";
+			echo " alert(\"添加成功\");\r\n";
+			echo " history.back();\r\n";
+			echo "</script>";
 		}
 	}
 
 	// 删除用户
 	public function delete()
 	{
-<<<<<<< HEAD
+		// include_once("Column.php");//引用处理类文件
+		// $f = new Column(); 
 		if (Session::has('id')) {
 			$student_id = Request::instance()->get('student_id');
 			$students = explode('_', $student_id);
 			foreach ($students as $id) {
 				Db::table('student')->where('id', $id)->delete();
 			}
-			$this->success("删除成功");
+			echo "<script language=\"JavaScript\">\r\n";
+			echo " history.back();\r\n";
+			echo "</script>";
 		} else {
-			$this->error("没有删除权限");
+			$this->error("没有删除权限,请登录",url('table/table'));
+			// $f->out();
 		}
 	}
 
 	// 更改用户信息
 	public function edit_user(){
-		
+		$id = Request::instance()->post('id');
+		$sex = Request::instance()->post('sex');
+		$username = Request::instance()->post('username');
+		$email = Request::instance()->post('email');
+		$phone = Request::instance()->post('phone');
+		$grade = Request::instance()->post('grade');
+		$department = Request::instance()->post('department');
+		$dorm = Request::instance()->post('dorm');
 
-
+		$result=Db('Student')
+		->where([
+			'id'  => $id
+		])
+		->update([
+			'sex' => $sex,
+			'username' => $username,
+			'email' => $email,
+			'phone' => $phone,
+			'grade' => $grade,
+			'department' => $department,
+			'dorm' => $dorm,
+		]);
+		if($result){
+			echo "<script language=\"JavaScript\">\r\n";
+			echo " alert(\"更新成功\");\r\n";
+			echo " history.back();\r\n";
+			echo "</script>";
+		}else{
+			echo "<script language=\"JavaScript\">\r\n";
+			echo " alert(\"更新失败\");\r\n";
+			echo " history.back();\r\n";
+			echo "</script>";
+		}	
 	}
-   
-=======
-		echo "suc";
-		$this->error();
-	}
 
-	
->>>>>>> 0d8647320324865129f370c2024e6edc207432e5
+	//上传，解析csv文件
+	public function uploadFile(){
+
+		// dump($_FILES["upfile"]);
+		if(is_uploaded_file($_FILES['upfile']['tmp_name'])){
+			$upfile=$_FILES["upfile"];
+			$name=$upfile['name'];
+			$tmp_name=$upfile["tmp_name"];//上传文件的临时存放路径
+			move_uploaded_file($tmp_name,$name);
+			// dump("上传成功");
+			}
+		else{
+			// dump("您还没有上传文件");
+		}	
+		if (!file_exists('uploads')){
+			mkdir('uploads');
+		}
+
+		$data = [];
+		$n = 0;
+		$file = fopen($name,'r');
+		while(! feof($file))
+		{
+			
+			if($n==0){
+				$n++;
+				$head = fgets($file);
+				$head = trim($head);
+				$head = explode(",",$head);
+				$head = mb_convert_encoding( $head, 'UTF-8', 'UTF-8,GBK,GB2312,BIG5' );
+				// dump($head);
+				// dump("--");
+			}
+			else{
+				$html = fgets($file);
+				$html = trim($html);
+				$html = explode(",",$html);
+				$reutrn_data[$n] = mb_convert_encoding( $html, 'UTF-8', 'UTF-8,GBK,GB2312,BIG5' );
+				$n++;
+				
+			}
+			
+		}
+		for($i=1;$i<$n-1;$i++){
+			$where = ['id' => $reutrn_data[$i][1]];
+			$data_id = Db('Student')->where($where)->find();
+			if(!$data_id){
+				$data[] = [
+					'id' => $reutrn_data[$i][1],
+					'password' => $reutrn_data[$i][1],
+					'sex' => $reutrn_data[$i][2],
+					'username' => $reutrn_data[$i][3],
+					'email' => $reutrn_data[$i][5],
+					'phone' => $reutrn_data[$i][4],
+					'grade' => $reutrn_data[$i][6],
+					'department' => $reutrn_data[$i][7],
+					'dorm' => $reutrn_data[$i][3],
+				];
+			}
+		}
+
+		if($data){
+			$result = Db('Student')->insertAll($data);
+			echo "<script language=\"JavaScript\">\r\n";
+			echo " alert(\"导入成功\");\r\n";
+			echo " history.back();\r\n";
+			echo "</script>";
+		}else{
+			echo "<script language=\"JavaScript\">\r\n";
+			echo " alert(\"导入失败\");\r\n";
+			echo " history.back();\r\n";
+			echo "</script>";
+		}
+		// dump($result);
+		fclose($file);
+	}
 }
