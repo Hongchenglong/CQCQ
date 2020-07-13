13d72fcc7ce1ba3c594af52d883caf19bb7da9c9
Qiiiii-u <947368746@qq.com>
2020-07-13 16:56:36 +0800

    统计

--- back_end/application/api/controller/BaseController.php
+++ back_end/application/api/controller/BaseController.php
@@ -25,6 +25,45 @@ class BaseController extends Controller
             }
         }
     }
+    public function request_post($url = '', $param = '') 
+    {
+        if (empty($url) || empty($param)) {
+            return false;
+        }
 
+        $postUrl = $url;
+        $curlPost = $param;
+        // 初始化curl
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $postUrl);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        // 要求结果为字符串且输出到屏幕上
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        // post提交方式
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $curlPost);
+        // 运行curl
+        $data = curl_exec($curl);
+        curl_close($curl);
 
+        return $data;
+    }
+
+    public function get_token() //获取access_token
+    {
+        $url = 'https://aip.baidubce.com/oauth/2.0/token';
+        $post_data['grant_type']       = 'client_credentials';
+        $post_data['client_id']      = 'GCjHu7F1LGrE2Dq1wm19rKGo';
+        $post_data['client_secret'] = 'TBLg5ROb7wvpLAk4gIOzM5VhWE2Hv48S';
+        $o = "";
+        foreach ($post_data as $k => $v) {
+            $o .= "$k=" . urlencode($v) . "&";
+        }
+        $post_data = substr($o, 0, -1);
+
+        $res = $this->request_post($url, $post_data);
+        $res = json_decode($res, true);
+        return $res['access_token'];
+    }
 }
--- back_end/application/api/controller/Resultsday.php
+++ back_end/application/api/controller/Resultsday.php
@@ -4,9 +4,10 @@ namespace app\api\controller;
 
 class Resultsday
 {
+    // 获取所有记录的日期
     public function getDay()
     {
-        // $parameter = ['department', 'grade', 'start_time', 'end_time'];
+        // $parameter = ['department', 'grade'];
         // 输入判断
         if (empty($_POST['grade'])) {
             $return_data = array();
@@ -33,12 +34,12 @@ class Resultsday
             ->where($where)
             ->distinct(true)
             ->select();
-        
+
         $day = array_column($day, 'start_time');
-        foreach($day as $k => $v){
+        foreach ($day as $k => $v) {
             $day[$k] = explode(' ', $day[$k])[0];
         }
-        
+
         $day = array_values(array_unique($day));
 
         $return_data = array();
@@ -46,6 +47,107 @@ class Resultsday
         $return_data['msg'] = '输出记录日期';
         $return_data['data']['day'] = $day;
         return json($return_data);
+    }
 
+    // 获取指定天数的所有记录的开始时间与结束时间
+    public function getDayRecord()
+    {
+        // $parameter = ['department', 'grade', 'time'];
+        // 输入判断
+        if (empty($_POST['grade'])) {
+            $return_data = array();
+            $return_data['error_code'] = 1;
+            $return_data['msg'] = '请输入年级！';
+            return json($return_data);
+        } else if (empty($_POST['department'])) {
+            $return_data = array();
+            $return_data['error_code'] = 1;
+            $return_data['msg'] = '请输入系！';
+            return json($return_data);
+        } else if (empty($_POST['time'])) {
+            $return_data = array();
+            $return_data['error_code'] = 1;
+            $return_data['msg'] = '请输入时间（年月日）！';
+            return json($return_data);
+        }
+
+        $where = array();
+        $where['s.grade'] = $_POST['grade'];
+        $where['s.department'] = $_POST['department'];
+        $where['r.deleted'] = 0;
+
+        $day = Db('record')
+            ->alias('r')    // 别名
+            ->field('r.start_time, r.end_time')
+            ->join('dorm d', 'd.id = r.dorm_id')
+            ->join('student s', 's.dorm = d.dorm_num')
+            ->where($where)
+            ->distinct(true)
+            ->select();
+
+        $day_ = array();
+        foreach ($day as $k => $v) {
+            if (substr($v['start_time'], 0, 10) == $_POST['time']) {
+                $day_[$k] = $v;
+            }
+        }
+
+        $day_ = array_values($day_); // 重新排序
+
+        $return_data = array();
+        $return_data['error_code'] = 0;
+        $return_data['msg'] = '输出指定天数记录！';
+        $return_data['data']['day'] = $day_;
+        return json($return_data);
+    }
+
+    // 获取指定月份的签到情况
+    public function stuSignSituation()
+    {
+        // $parameter = ['id', 'time'];
+        // 输入判断
+        if (empty($_POST['id'])) {
+            $return_data = array();
+            $return_data['error_code'] = 1;
+            $return_data['msg'] = '请输入学号！';
+            return json($return_data);
+        } else if (empty($_POST['time'])) {
+            $return_data = array();
+            $return_data['error_code'] = 1;
+            $return_data['msg'] = '请输入时间（年月）！';
+            return json($return_data);
+        }
+
+        $where = array();
+        $where['t.student_id'] = $_POST['id'];
+        $where['r.deleted'] = 0;
+
+        // 查找该学号所有的签到记录
+        $record = Db('record')
+            ->alias('r')
+            ->field('r.start_time, r.end_time, t.sign')
+            ->join('result t', 't.record_id = r.id')
+            ->where($where)
+            ->select();
+
+        $day = array();
+        foreach ($record as $k => $v) {
+            if (substr($v['start_time'], 0, 7) == $_POST['time']) {
+                $day[$k] = $v;
+            }
+        }
+
+        $day = array_values($day); // 重新排序
+
+        $record_num = count($day);
+        $sign_num = array_sum(array_column($day, 'sign'));
+        $percent = $sign_num/$record_num;
+        
+        $return_data = array();
+        $return_data['error_code'] = 0;
+        $return_data['msg'] = '输出指定月份签到情况！';
+        $return_data['data']['day'] = $day;
+        $return_data['data']['percent'] = $percent;
+        return json($return_data);
     }
 }
